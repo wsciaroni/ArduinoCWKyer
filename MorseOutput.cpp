33ccb016f33336f121ed6a03c78434271bf3b5a0
William Sciaroni <wsciaroni@protonmail.com>
2023-03-12 01:59:23 +0000

    Reformat MorseOutput class

--- MorseOutput.cpp
+++ MorseOutput.cpp
@@ -4,11 +4,11 @@
 
 void MorseOutput::sendStringAsCode(String &inputString)
 {
-    for(uint8_t i = 0; i < inputString.length(); i++)
-    {
-        sendMorseChar(inputString[i]);
-    }
-    Serial.println();
+  for (uint8_t i = 0; i < inputString.length(); i++)
+  {
+    sendMorseChar(inputString[i]);
+  }
+  Serial.println();
 }
 
 String MorseOutput::getUserInput()
@@ -16,8 +16,10 @@ String MorseOutput::getUserInput()
   // Prompt the user for input
   Serial.println("Enter message:");
 
-  //wait for data available
-  while (Serial.available() == 0) {}
+  // wait for data available
+  while (Serial.available() == 0)
+  {
+  }
 
   // Read the string
   String inputString = Serial.readString();
@@ -27,95 +29,94 @@ String MorseOutput::getUserInput()
 
   // Make uppercase string
   inputString.toUpperCase();
-  
+
   // Echo out the string to the terminal
   // Serial.println(inputString);
-  
+
   // Return the string that we received
   return inputString;
 }
 
-
 /**
   Either send a space or the character
 */
-void MorseOutput::sendMorseChar(const char& letter)
+void MorseOutput::sendMorseChar(const char &letter)
 {
   // Serial.print("In sendMorseChar: ");
   Serial.print(letter);
-  if(letter == ' ')
+  if (letter == ' ')
   {
-    delay(CW_SPACE_UNIT_LEN_MILLISECONDS);//there is a seven unit delay between words
+    delay(CW_SPACE_UNIT_LEN_MILLISECONDS); // there is a seven unit delay between words
   }
-  else 
+  else
   {
     byte bp = getMorseByte(letter);
     sendDawsAndDitsForChar(bp);
   }
 }
 
-
-char MorseOutput::getMorseByte(const char& letter)
+char MorseOutput::getMorseByte(const char &letter)
 {
   return morse_table[getCharIndex(letter)];
 }
 
-
-unsigned int MorseOutput::getCharIndex(const char& letter)
+unsigned int MorseOutput::getCharIndex(const char &letter)
 {
   static const unsigned int UPPER_LETTER_OFFSET = 'A' - 10;
   static const unsigned int LOWER_LETTER_OFFSET = 'a' - 10;
   static const unsigned int NUMBER_OFFSET = '0';
-  if(letter >= 'A' && letter <= 'Z') {
+  if (letter >= 'A' && letter <= 'Z')
+  {
     return letter - UPPER_LETTER_OFFSET;
   }
-  else if(letter >= 'a' && letter <= 'z')
+  else if (letter >= 'a' && letter <= 'z')
   {
     return letter - LOWER_LETTER_OFFSET;
   }
-  else if(letter >= '0' && letter <= '9')
+  else if (letter >= '0' && letter <= '9')
   {
     return letter - NUMBER_OFFSET;
   }
-  else if(letter == '#')
+  else if (letter == '#')
   {
     return 36;
   }
-  else if(letter == '+')
+  else if (letter == '+')
   {
     return 37;
   }
-  else if(letter == ',')
+  else if (letter == ',')
   {
     return 38;
   }
-  else if(letter == '-')
+  else if (letter == '-')
   {
     return 39;
   }
-  else if(letter == '.')
+  else if (letter == '.')
   {
     return 40;
   }
-  else if(letter == '/')
+  else if (letter == '/')
   {
     return 41;
   }
-  else if(letter == '=')
+  else if (letter == '=')
   {
     return 42;
   }
-  else if(letter == '?')
+  else if (letter == '?')
   {
     return 43;
   }
-  else if(letter == '^')
+  else if (letter == '^')
   {
     return 44;
   }
 }
 
-void MorseOutput::sendDawsAndDitsForChar(char bp) {
+void MorseOutput::sendDawsAndDitsForChar(char bp)
+{
   bool isFirstFound = false;
   bool isDaw = 0;
 
@@ -138,13 +139,13 @@ void MorseOutput::sendDawsAndDitsForChar(char bp) {
     else if (isDaw)
     {
       sendDaw();
-      //we have a one unit delay between parts of the letter
+      // we have a one unit delay between parts of the letter
       delay(CW_UNIT_LEN_MILLISECONDS);
     }
     else
     {
       sendDit();
-      //we have a one unit delay between parts of the letter
+      // we have a one unit delay between parts of the letter
       delay(CW_UNIT_LEN_MILLISECONDS);
     }
   }
@@ -153,12 +154,12 @@ void MorseOutput::sendDawsAndDitsForChar(char bp) {
   delay(CW_PARTIAL_SPACE_BETWEEN_CHARS);
 }
 
-bool MorseOutput::isFirstBitOne(const char& bp)
+bool MorseOutput::isFirstBitOne(const char &bp)
 {
   return (bp & 0b10000000) != 0;
 }
 
-void MorseOutput::shiftLeftOne(char& bp)
+void MorseOutput::shiftLeftOne(char &bp)
 {
   bp = bp << 1;
 }
@@ -177,7 +178,7 @@ void MorseOutput::MorseOutput::sendDaw()
 
 void MorseOutput::outputHighForDuration(int duration)
 {
-  digitalWrite(outputPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(duration);// wait for the set duration
-  digitalWrite(outputPin, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(outputPin, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(duration);               // wait for the set duration
+  digitalWrite(outputPin, LOW);  // turn the LED off by making the voltage LOW
 }
--- MorseOutput.h
+++ MorseOutput.h
@@ -6,20 +6,21 @@
 class MorseOutput
 {
 public:
-    MorseOutput(uint8_t pin) : outputPin(pin) {
+    MorseOutput(uint8_t pin) : outputPin(pin)
+    {
         pinMode(outputPin, OUTPUT);
     }
 
     ~MorseOutput() = default;
 
-    void sendStringAsCode(String& inputString);
+    void sendStringAsCode(String &inputString);
     String getUserInput();
 
 protected:
 private:
     /**
      * @brief Length of 1 unit of CW in Milliseconds
-     * 
+     *
      * Based on the word Paris:
      * | WPM | MILL |
      * | --- | ---- |
@@ -32,21 +33,21 @@ private:
      * |  30 |   40 |
      * |  40 |   30 |
      */
-    const int CW_UNIT_LEN_MILLISECONDS = 80;                                // Length of a dit is one unit
-    const int CW_DAW_UNIT_LEN_MILLISECONDS = CW_UNIT_LEN_MILLISECONDS*3;    // Lenght of a daw is 3 units
-    const int CW_SPACE_UNIT_LEN_MILLISECONDS = CW_UNIT_LEN_MILLISECONDS*7;  // Length between words is 7 units
-    const int CW_PARTIAL_SPACE_BETWEEN_CHARS = CW_UNIT_LEN_MILLISECONDS*2;
+    const int CW_UNIT_LEN_MILLISECONDS = 80;                                 // Length of a dit is one unit
+    const int CW_DAW_UNIT_LEN_MILLISECONDS = CW_UNIT_LEN_MILLISECONDS * 3;   // Lenght of a daw is 3 units
+    const int CW_SPACE_UNIT_LEN_MILLISECONDS = CW_UNIT_LEN_MILLISECONDS * 7; // Length between words is 7 units
+    const int CW_PARTIAL_SPACE_BETWEEN_CHARS = CW_UNIT_LEN_MILLISECONDS * 2;
 
     uint8_t outputPin;
 
-    void sendMorseChar(const char& letter);
-    char getMorseByte(const char& letter);
-    unsigned int getCharIndex(const char& letter);
+    void sendMorseChar(const char &letter);
+    char getMorseByte(const char &letter);
+    unsigned int getCharIndex(const char &letter);
 
     void sendDawsAndDitsForChar(char bp);
 
-    bool isFirstBitOne(const char& bp);
-    void shiftLeftOne(char& bp);
+    bool isFirstBitOne(const char &bp);
+    void shiftLeftOne(char &bp);
 
     void sendDit();
     void sendDaw();
